e4bee9a6d8322a0d1441a3c554a0ab5f7a6adca6
Shiv Singh <shivsingh7150@hotmail.com>
2023-04-03 17:27:05 +0530

    index

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
 
                 <Footer />
             </div>
-            <a href="#0" className="cd-top"> <i className="fas fa-angle-up"></i> </a>
+
             <Js />
         </>
     )
