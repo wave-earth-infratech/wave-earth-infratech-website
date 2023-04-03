ddbfdc584a2d8a9aebce7562dd21ee18aa20f55b
Shiv Singh <shivsingh7150@hotmail.com>
2023-04-03 17:39:21 +0530

    <Footer />

--- components/Footer.js
+++ components/Footer.js
@@ -90,11 +90,7 @@ export default function Footer() {
             </div>
         </div>
 
-        <div class="progress-wrap">
-            <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
-                <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;"></path>
-            </svg>
-        </div>
+        
 
 
         <div class="cart-bar">
