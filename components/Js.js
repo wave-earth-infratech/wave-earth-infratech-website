054942b92baee0604af1fa53d22d2a3bfbacbff9
Shiv Singh <shivsingh7150@hotmail.com>
2023-04-03 17:09:14 +0530

    js

--- components/Js.js
+++ components/Js.js
@@ -1,44 +1,44 @@
 export default function Js() {
   return (
     <>
-          <!-- jquery js -->
+
           <script src="/js/plugins/jquery.min.js"></script>
-          <!-- jquery ui -->
+
           <script src="/js/vendor/jqueryui.js"></script>
-          <!-- counter up -->
+
           <script src="/js/plugins/counter-up.js"></script>
           <script src="/js/plugins/swiper.js"></script>
-          <!-- twinmax -->
+
           <script src="/js/vendor/twinmax.js"></script>
-          <!-- split text js -->
+
           <script src="/js/vendor/split-text.js"></script>
-          <!-- text plugins -->
+
           <script src="/js/plugins/text-plugins.js"></script>
-          <!-- metismenu js -->
+
           <script src="/js/plugins/metismenu.js"></script>
-          <!-- waypoint js -->
+
           <script src="/js/vendor/waypoint.js"></script>
-          <!-- waw -->
+
           <script src="/js/vendor/waw.js"></script>
-          <!-- aos js -->
+
           <script src="/js/plugins/aos.js"></script>
-          <!-- jquery ui js -->
+
           <script src="/js/plugins/jquery-ui.js"></script>
-          <!-- timepickers -->
+
           <script src="/js/plugins/jquery-timepicker.js"></script>
-          <!-- sal animation -->
+
           <script src="/js/vendor/sal.min.js"></script>
-          <!-- bootstrap JS -->
+
           <script src="/js/plugins/bootstrap.min.js"></script>
-          <!-- easing JS -->
+
           <script src="/js/plugins/jquery-slideNav.js"></script>
-          <!-- easing JS -->
+
           <script src="/js/plugins/hover-revel.js"></script>
-          <!-- contact form js -->
+
           <script src="/js/plugins/contact-form.js"></script>
-          <!-- main js -->
+
           <script src="/js/main.js"></script>
-          <!-- swip image -->
+         
           <script src="/js/plugins/swip-img.js"></script>
     </>
   )
